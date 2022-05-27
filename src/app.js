37ce2e701f016c5fb55b40ec8dfe710a07247939
CarlosLukass <carlos.lukassf@gmail.com>
2022-05-27 06:43:02 +0000

    add Random Card Generator v1.0

--- src/app.js
+++ src/app.js
@@ -2,10 +2,100 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const cardType = ["♦", "♥", "♠", "♣"];
+const cardNumber = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "King",
+  "Queen",
+  "J",
+  "Ace"
+];
+let cards = 2;
+let cardIndex = 0;
+const $deck = document.querySelector("#deck");
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generateNewCard = () => {
+  //Disable animation in background card
+  const $card = document.querySelector(
+    cardIndex === 1 ? "#card0" : `#card${cardIndex - 1}`
+  );
+  if (cardIndex !== 0) {
+    $card.classList.remove("card-animation");
+  }
+
+  //Generate ramdon card data
+  const symbol = Math.floor(Math.random(cardType) * cardType.length);
+  const number = Math.floor(Math.random(cardNumber) * cardNumber.length);
+
+  //Generate new card
+  $deck.innerHTML += `<div
+                       id="card${cardIndex}"
+                       class="card-animation card-position position-absolute shadow bg-light rounded d-flex justify-content-between align-items-center flex-column px-2"
+                        style="width: 270px; height: 400px; z-index: 4; margin-top: -${cards}px">
+                       <div class="align-self-start">
+                         <h1 class="display-1 ${
+                           cardType[symbol] == "♦"
+                             ? "symbol-red"
+                             : cardType[symbol] == "♥"
+                             ? "symbol-red"
+                             : ""
+                         }">${cardType[symbol]}</h1>
+                       </div>
+                       <div>
+                          <h1 class="display-1 card-text">${
+                            cardNumber[number]
+                          }</h1>
+                                            </div>
+                       <div class="align-self-end">
+                         <h1 class="display-1 rotate ${
+                           cardType[symbol] == "♦"
+                             ? "symbol-red"
+                             : cardType[symbol] == "♥"
+                             ? "symbol-red"
+                             : ""
+                         }" >${cardType[symbol]}</h1>
+                       </div>
+                      </div>`;
+
+  // update variables
+  cardIndex += 1;
+  cards += 2;
+  console.log(cardIndex);
 };
+
+const timer = () => {
+  let segs = 10;
+  const $timer = document.querySelector("#timer");
+
+  setInterval(() => {
+    if (segs > 0) {
+      $timer.innerHTML = `&nbsp${segs} segs`;
+      segs -= 1;
+    } else {
+      generateNewCard();
+      segs = 10;
+    }
+  }, 1000);
+};
+
+// RENDER --------------------------------------
+
+// Load First Card
+window.onload = () => {
+  generateNewCard();
+  timer();
+};
+
+// add new card through button
+document.querySelector("#addNewCardBtn").addEventListener("click", () => {
+  generateNewCard();
+});
